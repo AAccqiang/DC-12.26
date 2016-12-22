7e90e48f1db7ce259b97b52e7239cb5792627478
AAccqiang <864090907@qq.com>
2016-12-22 17:54:10 +0800

    菜单页面

--- app/src/main/java/aa/edu/com/dc/activity/ChoosedishActivity.java
+++ app/src/main/java/aa/edu/com/dc/activity/ChoosedishActivity.java
@@ -43,7 +43,7 @@ public class ChoosedishActivity extends FragmentActivity {
     private ExpandableListView expandableListView;
     private DishMenuAdapter adapter;
 
-
+//adsa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
